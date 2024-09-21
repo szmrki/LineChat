8c4ef754334512b2a1e515bd510aaaf2fc6d7858
szmrki <suzumura.kei.x5@elms.hokudai.ac.jp>
2024-09-21 23:20:52 +0900

    20240921

--- lineapp.py
+++ lineapp.py
@@ -15,6 +15,7 @@ load_dotenv() #pyenv環境内ででAPIキーを取得するため
 #Lineのアクセストークンとシークレットを設定
 line_bot_api = LineBotApi(os.environ["LINE_BOT_API"])
 handler = WebhookHandler(os.environ["LINE_WEBHOOK_HANDLER"])
+package_id_list = [11537, 11538, 11539]  #LINEスタンプ用
 
 # ルートURLへのリクエストを処理する関数
 @app.route('/', methods=['GET', 'POST']) #ルートにGETかPOSTでアクセスされたときにhome関数を実行する
@@ -35,15 +36,17 @@ def handle_message(event):
     # ユーザーからのメッセージに対して応答
     text = generate_response(question)  #応答メッセージの作成
     texts = text.split('\n')     #応答メッセージに改行を含む場合、別の吹き出しとして送信するため分割
+    messages = [TextSendMessage(text=texts[i]) for i in range(len(texts))]  #複数メッセージ送信の際はTextSendMessageのリストを渡す
+    choice_stamp(text, messages)
+    
     line_bot_api.reply_message(
         event.reply_token,
-        [TextSendMessage(text=texts[i]) for i in range(len(texts))] #複数メッセージ送信の際はTextSendMessageのリストを渡す
+        messages=messages
     )
 
-#ユーザーからスタンプが送られてきたときの処理 とりあえずランダムでスタンプを送信
+#ユーザーからスタンプが送られてきたときの処理、ランダムでスタンプを送信
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_message(event):
-    package_id_list = [11537, 11538, 11539]
     num = random.randint(0, len(package_id_list)-1)
     if num == 0:
         sticker_id = random.randint(52002740, 52002773)
@@ -55,8 +58,36 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         #StickerSendMessage(package_id=event.message.package_id,sticker_id=event.message.sticker_id)   オウム返し
-        StickerSendMessage(package_id=package_id_list[num], sticker_id=sticker_id)
+        messages=StickerSendMessage(package_id=package_id_list[num], sticker_id=sticker_id)
     )
+
+#特定の文言に応じてスタンプを選択する関数
+def choice_stamp(text, messages):  
+    num = random.randint(0, len(package_id_list)-1)
+    if "了解" in text or "おっけー" in text:   
+        if num == 0:
+            sticker_ok = random.choice([52002735, 52002740])
+        elif num == 1:
+            sticker_ok = random.choice([51626500, 51626501, 51626520])
+        else :
+            sticker_ok = random.choice([52114113, 52114117])
+        messages.append(StickerSendMessage(package_id=package_id_list[num], sticker_id=sticker_ok)) #テキストとスタンプの同時送信のために、まとめてリストで渡す
+    if "泣" in text or "涙" in text:
+        if num == 0:
+            sticker_sad = 52002750
+        elif num == 1:
+            sticker_sad = random.choice([51626510, 51626522, 51626524, 51626529, 51626531])
+        else :
+            sticker_sad = random.choice([52114126, 52114137, 52114141, 52114145, 52114149])
+        messages.append(StickerSendMessage(package_id=package_id_list[num], sticker_id=sticker_sad))
+    if "すき！" in text or "ありがとう！" in text:
+        if num == 0:
+            sticker_love = random.choice([52002736, 52002737, 52002742, 52002743, 52002745, 52002747])
+        elif num == 1:
+            sticker_love = random.choice([51626495, 51626499, 51626502, 51626509])
+        else :
+            sticker_love = random.choice([52114111, 52114112, 52114118, 52114119, 52114124, 52114130, 52114147])
+        messages.append(StickerSendMessage(package_id=package_id_list[num], sticker_id=sticker_love))
 
 # 質問に対するレスポンスを生成する関数
 def generate_response(question):
